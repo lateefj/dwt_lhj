1c009b6d5387c7b60603c20d0305592a2894facd
Lateef Jackson <lateef.jackson@gmail.com>
2013-04-10 14:23:16 -0400

    Initial commit

---
+++ html_lists.dart
@@ -0,0 +1,52 @@
+part of dwt_lhj;
+
+class AbstracListPanel extends ui.ComplexPanel implements ui.InsertPanelForIsWidget {
+  /**
+   * Adds a new child widget to the panel.
+   *
+   * @param w the widget to be added
+   */
+  void add(ui.Widget w) {
+    addWidget(w, getElement());
+  }
+
+  void clear() {
+    try {
+      doLogicalClear();
+    } finally {
+      // Remove all existing child nodes.
+      for (Element element in getElement().children) {
+        element.remove();
+      }
+    }
+  }
+  void insertIsWidget(ui.IsWidget w, int beforeIndex) {
+    insertWidget(asWidgetOrNull(w), beforeIndex);
+  }
+
+  /**
+   * Inserts a widget before the specified index.
+   *
+   * @param w the widget to be inserted
+   * @param beforeIndex the index before which it will be inserted
+   * @throws IndexOutOfBoundsException if <code>beforeIndex</code>code> is out of
+   *           range
+   */
+  void insertWidget(ui.Widget w, int beforeIndex) {
+    insert(w, getElement(), beforeIndex, true);
+  }
+}
+/*
+ * Based on https://github.com/akserg/dart_web_toolkit/blob/master/lib/src/ui/flow_panel.dart 
+ */
+class LiPanel extends AbstracListPanel {
+  LiPanel() {
+    setElement(new LIElement());
+  }
+}
+
+class UlListPanel extends AbstracListPanel {
+  UlListPanel() {
+    setElement(new UListElement());
+  }
+}
---
+++ lhj.dart
@@ -0,0 +1,6 @@
+library dwt_lhj;
+import 'dart:html';
+import 'package:dart_web_toolkit/ui.dart' as ui;
+
+part 'html_lists.dart';
+
